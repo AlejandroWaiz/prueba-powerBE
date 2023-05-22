a8033f5ec627bdeb1d4916c8a1f8549791768470
Alejandro Waiz <alejosaurio1@gmail.com>
2023-05-22 19:40:17 -0400

    Changed vars to no sensitive info

---
+++ frontend/prueba/src/firebase/credentials.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+// Here is where you put your credentials :P 
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
